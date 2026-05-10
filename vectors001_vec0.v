5fab68a676e7bec6d1c551f588a08d11796cf73b
Mihir Dasgupta <dasguptamihir10@gmail.com>
2026-05-10 19:07:34 -0400

    Fix formatting of output wire declaration

--- vectors001_vec0.v
+++ vectors001_vec0.v
@@ -4,7 +4,7 @@ module top_module(input wire[2:0] vec
                   output wire[2:0] outv,
                   output wire o2,
                   output wire o1,
-                  output wire o0)
+                  output wire o0);
 
   assign outv = vec;
   assign o2 = vec[2];
