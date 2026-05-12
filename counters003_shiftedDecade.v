8d028875d70ee75a4cd4fb9a76aa2a367e1d808c
Mihir Dasgupta <dasguptamihir10@gmail.com>
2026-05-12 00:39:33 -0400

    Implement decade counter with reset functionality

---
+++ counters003_shiftedDecade.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output [3:0] q);
+    
+    always @(posedge clk) begin
+    
+        if(reset | q == 4'b1010) q <= 4'd1; // 4'b1010 = 4'd10
+        else q <= q + 4'd1;
+    
+    end
+
+endmodule
+
+// https://hdlbits.01xz.net/wiki/Count1to10
