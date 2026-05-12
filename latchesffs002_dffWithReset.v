27a3ae11d1782e48674b7eaafbe48ee2a9423ebe
Mihir Dasgupta <dasguptamihir10@gmail.com>
2026-05-11 21:35:52 -0400

    Add D flip-flop with synchronous reset module

---
+++ latchesffs002_dffWithReset.v
@@ -0,0 +1,17 @@
+// https://hdlbits.01xz.net/wiki/Dff8r
+
+module top_module (
+    input clk,
+    input reset,            // Synchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+
+    always @(posedge clk) begin
+        
+        if (reset == 1'b1) q <= 8'b0;
+        else q <= d;
+    
+    end
+    
+endmodule
